d1f98b4efcf72ce026f9a7893073e3a44d99503d
Ma Haozhe <haozhe.ma@u.nus.edu>
2023-10-12 17:12:56 +0800

    completed the SAC atari class

--- RLAlgos/SAC.py
+++ RLAlgos/SAC.py
@@ -39,7 +39,7 @@ class SAC:
 
     def __init__(self, env_id, actor_class, critic_class, exp_name="sac", render=False, seed=1, cuda=0, gamma=0.99,
                  buffer_size=1000000, rb_optimize_memory=False, batch_size=256, policy_lr=3e-4, q_lr=1e-3,
-                 alpha_lr=1e-4, target_network_frequency=1, tau=0.005, policy_frequency=2, noise_clip=0.5, alpha=0.2,
+                 alpha_lr=1e-4, target_network_frequency=1, tau=0.005, policy_frequency=2, alpha=0.2,
                  alpha_autotune=True, write_frequency=100, save_folder="./sac/"):
         """
         Initialize the SAC algorithm.
@@ -60,7 +60,6 @@ class SAC:
         :param tau: the soft update coefficient
         :param policy_frequency: the policy update frequency
         :param target_network_frequency: the target network update frequency
-        :param noise_clip: the noise clip
         :param alpha: the temperature parameter
         :param alpha_autotune: whether to autotune the temperature parameter
         :param write_frequency: the write frequency
@@ -123,7 +122,6 @@ class SAC:
         self.policy_frequency = policy_frequency
         self.target_network_frequency = target_network_frequency
         self.tau = tau
-        self.noise_clip = noise_clip
 
         # * for the tensorboard writer
         run_name = "{}-{}-{}".format(exp_name, seed,
@@ -286,9 +284,9 @@ class SAC_Atari(SAC):
     """
 
     def __init__(self, env_id, actor_class, critic_class, exp_name="sac", render=False, seed=1, cuda=0, gamma=0.99,
-                 buffer_size=1000000, rb_optimize_memory=False, batch_size=256, policy_lr=3e-4, q_lr=3e-4,
-                 alpha_lr=1e-4, target_network_frequency=8000, tau=1, policy_frequency=4, noise_clip=0.5, alpha=0.2,
-                 alpha_autotune=True, write_frequency=100, save_folder="./sac/"):
+                 buffer_size=1000000, rb_optimize_memory=False, batch_size=256, policy_lr=3e-4, q_lr=3e-4, eps=1e-4,
+                 alpha_lr=1e-4, target_network_frequency=8000, tau=1, policy_frequency=4, alpha=0.2,
+                 alpha_autotune=True, target_entropy_scale=0.89, write_frequency=100, save_folder="./sac/"):
         """
         Initialize the SAC algorithm.
         :param env_id: the name of the environment
@@ -304,20 +302,28 @@ class SAC_Atari(SAC):
         :param batch_size: the batch size
         :param policy_lr: the learning rate of the policy network
         :param q_lr: the learning rate of the Q network
+        :param eps: the epsilon for the Adam optimizer
         :param alpha_lr: the learning rate of the temperature parameter
         :param tau: the soft update coefficient
-        :param policy_frequency: the policy update frequency
+        :param policy_frequency: the frequency to UPDATE, DIFFERENT from the policy_frequency from SAC class
         :param target_network_frequency: the target network update frequency
-        :param noise_clip: the noise clip
         :param alpha: the temperature parameter
         :param alpha_autotune: whether to autotune the temperature parameter
+        :param target_entropy_scale: the scale of the target entropy
         :param write_frequency: the write frequency
         :param save_folder: the folder to save the model
         """
         super(SAC_Atari, self).__init__(env_id, actor_class, critic_class, exp_name, render, seed, cuda, gamma,
                                         buffer_size, rb_optimize_memory, batch_size, policy_lr, q_lr, alpha_lr,
-                                        target_network_frequency, tau, policy_frequency, noise_clip, alpha,
-                                        alpha_autotune, write_frequency, save_folder)
+                                        target_network_frequency, tau, policy_frequency, alpha, alpha_autotune,
+                                        write_frequency, save_folder)
+
+        self.q_optimizer = optim.Adam(list(self.qf_1.parameters()) + list(self.qf_2.parameters()), lr=q_lr, eps=eps)
+        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=policy_lr, eps=eps)
+
+        if alpha_autotune:
+            self.target_entropy = -target_entropy_scale * torch.log(1 / torch.tensor(self.env.action_space.n))
+            self.alpha_optimizer = optim.Adam([self.log_alpha], lr=alpha_lr, eps=eps)
 
     def make_env(self, env_id, seed, render):
         env = gym.make(env_id) if not render else gym.make(env_id, render_mode="human")
@@ -337,3 +343,70 @@ class SAC_Atari(SAC):
         env = gym.wrappers.RecordEpisodeStatistics(env)
 
         return env
+
+    def optimize(self, global_step):
+        if global_step % self.policy_frequency == 0:
+            data = self.replay_buffer.sample(self.batch_size)
+
+            with torch.no_grad():
+                _, next_state_log_pi, next_state_action_probs = self.actor.get_action(data.next_observations)
+                qf_1_next_target = self.qf_1_target(data.next_observations)
+                qf_2_next_target = self.qf_2_target(data.next_observations)
+                # we can use the action probabilities instead of MC sampling to estimate the expectation
+                min_qf_next_target = next_state_action_probs * (
+                        torch.min(qf_1_next_target, qf_2_next_target) - self.alpha * next_state_log_pi).sum(dim=1)
+                next_q_value = data.rewards.flatten() + \
+                               (1 - data.dones.flatten()) * self.gamma * (min_qf_next_target)
+
+            qf_1_values = self.qf_1(data.observations)
+            qf_2_values = self.qf_2(data.observations)
+
+            qf_1_a_values = qf_1_values.gather(1, data.actions).view(-1)
+            qf_2_a_values = qf_2_values.gather(1, data.actions).view(-1)
+            qf_1_loss = F.mse_loss(qf_1_a_values, next_q_value)
+            qf_2_loss = F.mse_loss(qf_2_a_values, next_q_value)
+            qf_loss = qf_1_loss + qf_2_loss
+
+            self.q_optimizer.zero_grad()
+            qf_loss.backward()
+            self.q_optimizer.step()
+
+            # for actor training
+            _, log_pi, action_probs = self.actor.get_action(data.observations)
+            with torch.no_grad():
+                qf_1_values = self.qf_1(data.observations)
+                qf_2_values = self.qf_2(data.observations)
+                min_qf_values = torch.min(qf_1_values, qf_2_values)
+            actor_loss = (action_probs * (self.alpha * log_pi) - min_qf_values).mean()
+
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
+
+            if self.alpha_autotune:
+                # re-use action probabilities for temperature loss
+                alpha_loss = (action_probs.detach() * (
+                            -self.log_alpha.exp() * (log_pi + self.target_entropy).detach())).mean()
+
+                self.alpha_optimizer.zero_grad()
+                alpha_loss.backward()
+                self.alpha_optimizer.step()
+                self.alpha = self.log_alpha.exp().item()
+
+            # update the target networks
+            if global_step % self.target_network_frequency == 0:
+                for param, target_param in zip(self.qf_1.parameters(), self.qf_1_target.parameters()):
+                    target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+                for param, target_param in zip(self.qf_2.parameters(), self.qf_2_target.parameters()):
+                    target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+            if global_step % self.write_frequency == 0:
+                self.writer.add_scalar("losses/qf_1_values", qf_1_a_values.mean().item(), global_step)
+                self.writer.add_scalar("losses/qf_2_values", qf_2_a_values.mean().item(), global_step)
+                self.writer.add_scalar("losses/qf_1_loss", qf_1_loss.item(), global_step)
+                self.writer.add_scalar("losses/qf_2_loss", qf_2_loss.item(), global_step)
+                self.writer.add_scalar("losses/qf_loss", qf_loss.item() / 2.0, global_step)
+                self.writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
+                self.writer.add_scalar("losses/alpha", self.alpha, global_step)
+                if self.alpha_autotune:
+                    self.writer.add_scalar("losses/alpha_loss", alpha_loss.item(), global_step)
---
+++ run-scripts/sac-atari.py
@@ -0,0 +1,88 @@
+"""
+The script to SAC on Atari environments.
+"""
+
+import argparse
+
+from RLAlgos.SAC import SAC_Atari
+
+from Networks.ActorNetworks import SACActorAtari
+from Networks.QNetworks import SACSoftQNetworkAtari
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run SAC on Atari environments.")
+
+    parser.add_argument("--exp-name", type=str, default="sac-atari")
+
+    parser.add_argument("--env-id", type=str, default="ALE/Breakout-v5")
+    parser.add_argument("--render", type=bool, default=False)
+
+    parser.add_argument("--seed", type=int, default=1)
+    parser.add_argument("--cuda", type=int, default=0)
+    parser.add_argument("--gamma", type=float, default=0.99)
+
+    parser.add_argument("--buffer-size", type=int, default=1000000)
+    parser.add_argument("--rb-optimize-memory", type=bool, default=True)
+    parser.add_argument("--batch-size", type=int, default=64)
+
+    parser.add_argument("--policy-lr", type=float, default=3e-4)
+    parser.add_argument("--q-lr", type=float, default=3e-3)
+    parser.add_argument("--alpha-lr", type=float, default=1e-4)
+    parser.add_argument("--eps", type=float, default=1e-4)
+
+    parser.add_argument("--target-network-frequency", type=int, default=8000)
+    parser.add_argument("--tau", type=float, default=1.0)
+
+    parser.add_argument("--policy-frequency", type=int, default=4)
+
+    parser.add_argument("--alpha", type=float, default=0.2)
+    parser.add_argument("--alpha-autotune", type=bool, default=True)
+    parser.add_argument("--target-entropy-scale", type=float, default=0.89)
+
+    parser.add_argument("--write-frequency", type=int, default=100)
+    parser.add_argument("--save-folder", type=str, default="./sac/")
+
+    parser.add_argument("--total-timesteps", type=int, default=5000000)
+    parser.add_argument("--learning-starts", type=int, default=2e4)
+
+    args = parser.parse_args()
+    return args
+
+
+def run():
+    args = parse_args()
+
+    agent = SAC_Atari(
+        env_id=args.env_id,
+        actor_class=SACActorAtari,
+        critic_class=SACSoftQNetworkAtari,
+        exp_name=args.exp_name,
+        render=args.render,
+        seed=args.seed,
+        cuda=args.cuda,
+        gamma=args.gamma,
+        buffer_size=args.buffer_size,
+        rb_optimize_memory=args.rb_optimize_memory,
+        batch_size=args.batch_size,
+        policy_lr=args.policy_lr,
+        q_lr=args.q_lr,
+        eps=args.eps,
+        alpha_lr=args.alpha_lr,
+        target_network_frequency=args.target_network_frequency,
+        tau=args.tau,
+        policy_frequency=args.policy_frequency,
+        alpha=args.alpha,
+        alpha_autotune=args.alpha_autotune,
+        target_entropy_scale=args.target_entropy_scale,
+        write_frequency=args.write_frequency,
+        save_folder=args.save_folder
+    )
+
+    agent.learn(total_timesteps=args.total_timesteps, learning_starts=args.learning_starts)
+
+    agent.save(indicator="final")
+
+
+if __name__ == "__main__":
+    run()
