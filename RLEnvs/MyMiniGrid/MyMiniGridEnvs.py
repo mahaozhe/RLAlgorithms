b201e8ca70dd1b3e231f17600df52119a3b8ac45
Ma Haozhe <haozhe.ma@u.nus.edu>
2024-02-06 21:16:14 +0800

    update the minigrid to the latest version, to be compatible with the render mode "human"

--- RLEnvs/MyMiniGrid/MyMiniGridEnvs.py
+++ RLEnvs/MyMiniGrid/MyMiniGridEnvs.py
@@ -15,7 +15,7 @@ class MyNormalRoomsEnv(MiniGridEnv):
     Self-defined normal room environments.
     """
 
-    def __init__(self, width, height, walls_list, max_steps, agent_init_loc=(1, 1), goal_loc=None, **kwargs):
+    def __init__(self, width, height, walls_list, max_steps, agent_init_loc=None, goal_loc=None, **kwargs):
         """
         Init function.
         :param width: the width of the maze.
@@ -27,53 +27,46 @@ class MyNormalRoomsEnv(MiniGridEnv):
         """
 
         self.walls_list = walls_list
-        self.agent_init_loc = agent_init_loc
-        self.goal_loc = goal_loc
+        self._agent_default_pos = agent_init_loc if agent_init_loc is not None else (1, 1)
+        self._goal_default_pos = goal_loc if goal_loc is not None else (width - 2, height - 2)
 
         mission_space = MissionSpace(mission_func=self._gen_mission)
 
         super(MyNormalRoomsEnv, self).__init__(mission_space=mission_space, width=width, height=height,
                                                max_steps=max_steps, **kwargs)
 
-    def _gen_mission(self):
+    @staticmethod
+    def _gen_mission():
         return "reach the goal"
 
     def _gen_grid(self, width, height):
         # to create an empty grid
         self.grid = Grid(width, height)
-        # to create the surrounding walls
-        self.grid.wall_rect(0, 0, width, height)
+
+        # Generate the surrounding walls
+        self.grid.horz_wall(0, 0)
+        self.grid.horz_wall(0, height - 1)
+        self.grid.vert_wall(0, 0)
+        self.grid.vert_wall(width - 1, 0)
 
         for wall in self.walls_list:
-            self.put_obj(Wall(), wall[0], wall[1])
+            self.grid.set(wall[0], wall[1], Wall())
 
-        self.agent_pos = self.agent_init_loc
-
+        self.agent_pos = self._agent_default_pos
+        self.grid.set(*self._agent_default_pos, None)
         self.agent_dir = 0
 
-        if self.goal_loc is not None:
-            self.put_obj(Goal(), self.goal_loc[0], self.goal_loc[1])
-        else:
-            # put the goal state at the right-lower conner by default.
-            self.put_obj(Goal(), width - 2, height - 2)
+        self.put_obj(Goal(), *self._goal_default_pos)
 
 
 # + For the "four-room" environment:
 env_four_room = [(6, 1), (6, 2), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8), (6, 9), (6, 11), (6, 12),
                  (1, 6), (3, 6), (4, 6), (5, 6), (7, 7), (8, 7), (10, 7), (11, 7)]
 
-
-class EnvFourRoom(MyNormalRoomsEnv):
-    def __init__(self):
-        super(EnvFourRoom, self).__init__(width=13,
-                                          height=13,
-                                          walls_location_list=env_four_room,
-                                          max_steps=1000)
-
-
 register(
     id='MiniGrid-Env-Four-Room',
-    entry_point='Envs.MiniGrid.Envs:EnvFourRoom'
+    entry_point='RLEnvs.MyMiniGrid.MyMiniGridEnvs:MyNormalRoomsEnv',
+    kwargs={"width": 13, "height": 13, "walls_list": env_four_room, "max_steps": 1000},
 )
 
 
@@ -91,7 +84,7 @@ class SelfDefinedKeyDoorEnv(MiniGridEnv):
     The class to define an environment with multiple key-doors
     """
 
-    def __init__(self, width, height, objects_loc, max_steps, agent_init_loc=(1, 1), goal_loc=None, **kwargs):
+    def __init__(self, width, height, objects_loc, max_steps, agent_init_loc=None, goal_loc=None, **kwargs):
         """
         Init function.
         :param width: the width of the maze.
@@ -102,11 +95,12 @@ class SelfDefinedKeyDoorEnv(MiniGridEnv):
         :param goal_loc: the location of the goal state.
         """
 
-        self.wall_locs = objects_loc['walls']
+        self.walls_list = objects_loc['walls']
         self.door_locs = objects_loc['doors']
         self.key_locs = objects_loc['keys']
-        self.agent_init_loc = agent_init_loc
-        self.goal_loc = goal_loc
+
+        self._agent_default_pos = agent_init_loc if agent_init_loc is not None else (1, 1)
+        self._goal_default_pos = goal_loc if goal_loc is not None else (width - 2, height - 2)
 
         self.colors = ["yellow", "blue", "green"]
 
@@ -115,39 +109,37 @@ class SelfDefinedKeyDoorEnv(MiniGridEnv):
         super(SelfDefinedKeyDoorEnv, self).__init__(mission_space=mission_space, width=width, height=height,
                                                     max_steps=max_steps, **kwargs)
 
-    def _gen_mission(self):
+    @staticmethod
+    def _gen_mission():
         return "reach the goal"
 
     def _gen_grid(self, width, height):
         # to create an empty grid
         self.grid = Grid(width, height)
-        # to create the surrounding walls
-        self.grid.wall_rect(0, 0, width, height)
 
-        # to place the goal
-        if self.goal_loc is not None:
-            self.put_obj(Goal(), self.goal_loc[0], self.goal_loc[1])
-        else:
-            # put the goal state at the right-lower conner by default.
-            self.put_obj(Goal(), width - 2, height - 2)
+        # Generate the surrounding walls
+        self.grid.horz_wall(0, 0)
+        self.grid.horz_wall(0, height - 1)
+        self.grid.vert_wall(0, 0)
+        self.grid.vert_wall(width - 1, 0)
 
-        # to place the aget at its initial location:
-        self.agent_pos = self.agent_init_loc
-
-        # to rotate the agent to an expected direction
+        self.agent_pos = self._agent_default_pos
+        self.grid.set(*self._agent_default_pos, None)
         self.agent_dir = 0
 
+        self.put_obj(Goal(), *self._goal_default_pos)
+
         # to place the walls
-        for wall in self.wall_locs:
-            self.put_obj(Wall(), wall[0], wall[1])
+        for wall in self.walls_list:
+            self.grid.set(wall[0], wall[1], Wall())
 
         # to place the door
         for d in range(len(self.door_locs)):
-            self.put_obj(Door(self.colors[d], is_locked=True), self.door_locs[d][0], self.door_locs[d][1])
+            self.grid.set(self.door_locs[d][0], self.door_locs[d][1], Door(self.colors[d], is_locked=True))
 
         # to place the key
         for k in range(len(self.key_locs)):
-            self.place_obj(obj=MyKey(self.colors[k]), top=(self.key_locs[k][0], self.key_locs[k][1]), size=(1, 1))
+            self.grid.set(self.key_locs[k][0], self.key_locs[k][1], MyKey(self.colors[k]))
 
 
 # + the "key-door" environment with two pairs of key-doors
@@ -156,18 +148,9 @@ env_key_door = {"walls": [(8, 1), (8, 2), (8, 3), (8, 5), (8, 6), (8, 7), (8, 9)
                 "doors": [(8, 4), (8, 8)],
                 "keys": [(4, 9), (4, 3)]}
 
-
-class EnvKeyDoor(SelfDefinedKeyDoorEnv):
-    def __init__(self):
-        super(EnvKeyDoor, self).__init__(width=13,
-                                         height=13,
-                                         objects_loc=env_key_door,
-                                         max_steps=2000,
-                                         agent_init_loc=(1, 6),
-                                         goal_loc=(11, 6))
-
-
 register(
     id='MiniGrid-Env-Key-Door',
-    entry_point='Envs.MiniGrid.Envs:EnvKeyDoor'
+    entry_point='RLEnvs.MyMiniGrid.MyMiniGridEnvs:SelfDefinedKeyDoorEnv',
+    kwargs={"width": 13, "height": 13, "objects_loc": env_key_door, "max_steps": 2000, "agent_init_loc": (1, 6),
+            "goal_loc": (11, 6)}
 )
