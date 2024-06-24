a08720d25ac1712c7fcb4ff6c7089392b358b19a
Ma Haozhe <haozhe.ma@u.nus.edu>
2024-06-24 21:51:31 +0800

    update the key-door envs for new versions

--- RLEnvs/MyMiniGrid/MyMiniGridEnvs.py
+++ RLEnvs/MyMiniGrid/MyMiniGridEnvs.py
@@ -98,7 +98,7 @@ class MyKey(Key):
         return True
 
 
-class SelfDefinedKeyDoorEnv(MiniGridEnv):
+class MyKeyDoorEnvSelfDefinedKey(MiniGridEnv):
     """
     The class to define an environment with multiple key-doors
     """
@@ -125,8 +125,8 @@ class SelfDefinedKeyDoorEnv(MiniGridEnv):
 
         mission_space = MissionSpace(mission_func=self._gen_mission)
 
-        super(SelfDefinedKeyDoorEnv, self).__init__(mission_space=mission_space, width=width, height=height,
-                                                    max_steps=max_steps, **kwargs)
+        super(MyKeyDoorEnvSelfDefinedKey, self).__init__(mission_space=mission_space, width=width, height=height,
+                                                         max_steps=max_steps, **kwargs)
 
     @staticmethod
     def _gen_mission():
@@ -168,8 +168,54 @@ env_key_door = {"walls": [(8, 1), (8, 2), (8, 3), (8, 5), (8, 6), (8, 7), (8, 9)
                 "keys": [(4, 9), (4, 3)]}
 
 register(
-    id='MiniGrid-Env-Key-Door',
-    entry_point='RLEnvs.MyMiniGrid.MyMiniGridEnvs:SelfDefinedKeyDoorEnv',
+    id='MiniGrid-Env-Two-Key-Door',
+    entry_point='RLEnvs.MyMiniGrid.MyMiniGridEnvs:MyKeyDoorEnvSelfDefinedKey',
     kwargs={"width": 13, "height": 13, "objects_loc": env_key_door, "max_steps": 2000, "agent_init_loc": (1, 6),
             "goal_loc": (11, 6)}
 )
+
+AvailableColors = ["yellow", "blue", "green", "red", "purple"]
+
+
+class MyDoorKeyEnv(MiniGridEnv):
+    """
+    Environment with a key (keys) and the corresponding door (doors).
+    """
+
+    def __init__(self, width, height, obj_lists, max_steps, agent_init_loc=None, goal_loc=None, **kwargs):
+        self.obj_lists = obj_lists
+        self._agent_default_pos = agent_init_loc if agent_init_loc is not None else (1, 1)
+        self._goal_default_pos = goal_loc if goal_loc is not None else (width - 2, height - 2)
+
+        mission_space = MissionSpace(mission_func=self._gen_mission)
+        super(MyDoorKeyEnv, self).__init__(mission_space=mission_space, width=width, height=height, max_steps=max_steps,
+                                           **kwargs)
+
+    @staticmethod
+    def _gen_mission():
+        return "use the key to open the door and then get to the goal"
+
+    def _gen_grid(self, width, height):
+        # to create an empty grid
+        self.grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.wall_rect(0, 0, width, height)
+
+        self.agent_pos = self._agent_default_pos
+        self.grid.set(*self._agent_default_pos, None)
+        self.agent_dir = 0
+
+        self.put_obj(Goal(), *self._goal_default_pos)
+
+        for wall in self.obj_lists['walls']:
+            self.grid.set(wall[0], wall[1], Wall())
+
+        for d in range(len(self.obj_lists['doors'])):
+            self.put_obj(Door(AvailableColors[d], is_locked=True),
+                         self.obj_lists['doors'][d][0], self.obj_lists['doors'][d][1])
+
+        for k in range(len(self.obj_lists['keys'])):
+            self.place_obj(obj=Key(AvailableColors[k]),
+                           top=(self.obj_lists['keys'][k][0], self.obj_lists['keys'][k][1]),
+                           size=(1, 1))
